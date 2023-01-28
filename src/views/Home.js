9d1cf94326d071242122f0942b4f48a3730c696b
Ben Armstead <ben@benarmstead.co.uk>
2023-01-28 12:41:28 +0000

    Populate site with real data

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,24 +1,23 @@
 import React from "react";
+import Footer from "../components/layout/Footer";
 // import sections
-import Hero from "../components/sections/Hero";
-import FeaturesTiles from "../components/sections/FeaturesTiles";
 import FeaturesSplit from "../components/sections/FeaturesSplit";
-import Testimonial from "../components/sections/Testimonial";
-import Cta from "../components/sections/Cta";
+import Hero from "../components/sections/Hero";
 
 const Home = () => {
   return (
     <>
       <Hero className="illustration-section-01" />
-      <FeaturesTiles />
+      {/* <FeaturesTiles /> */}
       <FeaturesSplit
         invertMobile
         topDivider
         imageFill
         className="illustration-section-02"
       />
-      <Testimonial topDivider />
-      <Cta split />
+      {/* <Testimonial topDivider />
+      <Cta split /> */}
+      <Footer />
     </>
   );
 };
