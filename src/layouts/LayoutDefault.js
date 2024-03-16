9ae8552f7c45ee0f423fd27b83db0cbe1b06cac7
benarmstead <ben@benarmstead.co.uk>
2024-03-16 22:27:55 +0000

    Remove header

--- src/layouts/LayoutDefault.js
+++ src/layouts/LayoutDefault.js
@@ -1,10 +1,8 @@
 import React from "react";
 import Footer from "../components/layout/Footer";
-import Header from "../components/layout/Header";
 
 const LayoutDefault = ({ children }) => (
   <>
-    <Header navPosition="right" className="reveal-from-bottom" hideSignin />
     <main className="site-content">{children}</main>
     {/* <Banner /> */}
     <Footer />
